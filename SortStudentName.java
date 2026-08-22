d3f46c78a0ecfc60fcde19ddfd4f46c247dcf2f8
Himanshu-Vinchurkar12 <himanshuvinchurkar111@gmail.com>
2026-08-22 21:57:32 +0530

    Sort Student Name

--- SortStudentName.java
+++ SortStudentName.java
@@ -1,21 +1,24 @@
 import java.util.*;
+
 public class SortStudentName {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       List<String> studeList = new ArrayList<>();
-       
-       System.out.println("Enter upto 10 student name:");
-       while (studeList.size()<10) {
-         System.out.print("Enter name "+(studeList.size()+1)+" is: ");
-         String student = sc.nextLine().trim();
-         studeList.add(student);
-       }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    List<String> studeList = new ArrayList<>();
 
-       Collections.sort(studeList);
-
-       System.out.println("Sorted list of Student: ");
-      for (String string : studeList) {
-        System.out.println(string);
-      }
+    System.out.println("Enter upto 10 student name:");
+    while (studeList.size() < 10) {
+      System.out.print("Enter name " + (studeList.size() + 1) + " is: ");
+      String student = sc.nextLine().trim();
+      studeList.add(student);
     }
+
+    Collections.sort(studeList);
+
+    System.out.println("Sorted list of Student: ");
+    for (String string : studeList) {
+      System.out.println(string);
+    }
+
+    sc.close();
+  }
 }
